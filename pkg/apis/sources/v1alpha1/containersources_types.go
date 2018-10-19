d0a0833f093bc78a7482faa46092f5d632ffaef0
Scott Nichols <nicholss@google.com>
2018-10-19 08:40:55 -0700

    adding spec to container source.

--- pkg/apis/sources/v1alpha1/containersources_types.go
+++ pkg/apis/sources/v1alpha1/containersources_types.go
@@ -17,22 +17,80 @@ limitations under the License.
 package v1alpha1
 
 import (
+	duckv1alpha1 "github.com/knative/pkg/apis/duck/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// Important: Run "make" to regenerate code after modifying this file
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // ContainerSourcesSpec defines the desired state of ContainerSources
 type ContainerSourcesSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Image is the image to run inside of the container.
+	// +kubebuilder:validation:MinLength=1
+	Image string `json:"image,omitempty"`
+
+	// Args are passed to the ContainerSpec as they are.
+	Args []string `json:"args,omitempty"`
+
+	// Sink is a reference to an object that will resolve to a domain name to use as the sink.
+	// +optional
+	Sink *corev1.ObjectReference `json:"sink,omitempty"`
 }
+
+const (
+	// ContainerSourceConditionReady has status True when the ContainerSource is ready to send events.
+	ContainerConditionReady = duckv1alpha1.ConditionReady
+
+	// ContainerConditionSinkProvided has status True when the ContainerSource has been configured with a sink target.
+	ContainerConditionSinkProvided duckv1alpha1.ConditionType = "SinkProvided"
+)
+
+var containerCondSet = duckv1alpha1.NewLivingConditionSet(ContainerConditionSinkProvided)
 
 // ContainerSourcesStatus defines the observed state of ContainerSources
 type ContainerSourcesStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Conditions holds the state of a source at a point in time.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions duckv1alpha1.Conditions `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+
+	// TODO(n3wscott): there was talk of adding sink to the status. revisit this later.
+
+	// SinkURI is the current active sink URI that has been configured for the ContainerSource.
+	SinkURI string `json:"sinkUri,omitempty"`
+}
+
+// GetCondition returns the condition currently associated with the given type, or nil.
+func (s *ContainerSourcesStatus) GetCondition(t duckv1alpha1.ConditionType) *duckv1alpha1.Condition {
+	return containerCondSet.Manage(s).GetCondition(t)
+}
+
+// IsReady returns true if the resource is ready overall.
+func (s *ContainerSourcesStatus) IsReady() bool {
+	return containerCondSet.Manage(s).IsHappy()
+}
+
+// InitializeConditions sets relevant unset conditions to Unknown state.
+func (s *ContainerSourcesStatus) InitializeConditions() {
+	containerCondSet.Manage(s).InitializeConditions()
+}
+
+// MarSink sets the condition that the source has a sink configured.
+func (s *ContainerSourcesStatus) MarkSink(uri string) {
+	s.SinkURI = uri
+	if len(uri) > 0 {
+		containerCondSet.Manage(s).MarkTrue(ContainerConditionSinkProvided)
+	} else {
+		containerCondSet.Manage(s).MarkUnknown(ContainerConditionSinkProvided, "SinkEmpty", "Sink has resolved to empty.")
+	}
+}
+
+// MarkNoSink sets the condition that the source does not have a sink configured.
+func (s *ContainerSourcesStatus) MarkNoSink(reason, messageFormat string, messageA ...interface{}) {
+	containerCondSet.Manage(s).MarkFalse(ContainerConditionSinkProvided, reason, messageFormat, messageA)
 }
 
 // +genclient
