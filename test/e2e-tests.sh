e1af29e463f44c057bc567a12c99a73ec35d7bab
Scott Nichols <32305648+n3wscott@users.noreply.github.com>
2018-10-19 10:44:19 -0700

    adding a test shell dir (#7)
    * adding a test shell dir
    
    * Update image_paths.txt
    
    * Update test/image_paths.txt
    
    Co-Authored-By: n3wscott <nicholss@google.com>
    
    * Update test/e2e-tests.sh
    
    Co-Authored-By: n3wscott <nicholss@google.com>
    
    * Update test/e2e-tests.sh
    
    Co-Authored-By: n3wscott <nicholss@google.com>
    
    * Update test/README.md
    
    Co-Authored-By: n3wscott <nicholss@google.com>
    
    * Update test/README.md
    
    Co-Authored-By: n3wscott <nicholss@google.com>
    
    * Update test/README.md
    
    Co-Authored-By: n3wscott <nicholss@google.com>

---
+++ test/e2e-tests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copyright 2018 The Knative Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script runs the end-to-end tests against the eventing sources
+# built from source.
+
+# If you already have the *_OVERRIDE environment variables set, call
+# this script with the --run-tests arguments and it will use the cluster
+# and run the tests.
+
+# Calling this script without arguments will create a new cluster in
+# project $PROJECT_ID, start Knative serving and the eventing system, run
+# the tests and delete the cluster.
+
+source $(dirname $0)/../vendor/github.com/knative/test-infra/scripts/e2e-tests.sh
+
+# Script entry point.
+
+initialize $@
+
+# TODO(#8): Write E2E tests.
+
+success
