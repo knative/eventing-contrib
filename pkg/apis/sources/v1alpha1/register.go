eaaf51a6de99444c066714d81d904f8d3d0ced37
Scott Nichols <nicholss@google.com>
2018-10-22 15:37:09 -0700

    Don't need addKnownTypes.

--- pkg/apis/sources/v1alpha1/register.go
+++ pkg/apis/sources/v1alpha1/register.go
@@ -25,8 +25,6 @@ limitations under the License.
 package v1alpha1
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/scheme"
 )
@@ -44,14 +42,4 @@ var (
 // Resource takes an unqualified resource and returns a Group qualified GroupResource
 func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
-}
-
-// Adds the list of known types to Scheme.
-func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(SchemeGroupVersion,
-		&ContainerSource{},
-		&ContainerSourceList{},
-	)
-	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
-	return nil
 }
