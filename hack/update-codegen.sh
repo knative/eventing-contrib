f35ae5d44165fa1212f3259517101455e365fad0
Scott Nichols <nicholss@google.com>
2018-10-24 07:21:58 -0700

    update verison of apiserver.

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -22,6 +22,9 @@ source $(dirname $0)/../vendor/github.com/knative/test-infra/scripts/library.sh
 
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
 
+# Generate based on annotations
+go generate ./pkg/... ./cmd/...
+
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
