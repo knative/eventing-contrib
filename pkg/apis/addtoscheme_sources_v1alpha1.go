48bcf96b98156053a84447f0b26973b9de50db64
Ben Browning <bbrownin@redhat.com>
2018-11-02 18:10:48 -0400

    Add servingv1alpha1 to the controller-manager's scheme
    I'm not sure this is the best way to do this, but controller-manager
    needs the serving.knative.dev v1alpha1 scheme registered and this
    seemed like the appropriate place to do that.

--- pkg/apis/addtoscheme_sources_v1alpha1.go
+++ pkg/apis/addtoscheme_sources_v1alpha1.go
@@ -18,9 +18,10 @@ package apis
 
 import (
 	"github.com/knative/eventing-sources/pkg/apis/sources/v1alpha1"
+	servingv1alpha1 "github.com/knative/serving/pkg/apis/serving/v1alpha1"
 )
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
-	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme, servingv1alpha1.SchemeBuilder.AddToScheme)
 }
