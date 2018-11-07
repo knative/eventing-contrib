0772dac7b149ee6b253b75a9e31765a138714e25
Ben Browning <bbrownin@redhat.com>
2018-11-07 12:54:00 -0500

    Oops - wrong Kubernetes "sets" import

--- pkg/controller/githubsource/reconcile.go
+++ pkg/controller/githubsource/reconcile.go
@@ -33,10 +33,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/record"
-	"k8s.io/kubernetes/pkg/util/sets"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
