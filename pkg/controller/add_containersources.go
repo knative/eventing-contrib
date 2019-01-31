16f8d4b77f23f873a0b4384c407ff118e5349ea4
Scott Nichols <nicholss@google.com>
2019-01-31 11:31:06 -0800

    Move the rest of reconcilers.

--- pkg/controller/add_containersources.go
+++ pkg/controller/add_containersources.go
@@ -17,7 +17,7 @@ limitations under the License.
 package controller
 
 import (
-	"github.com/knative/eventing-sources/pkg/controller/containersource"
+	"github.com/knative/eventing-sources/pkg/reconciler/containersource"
 )
 
 func init() {
