647041b01c2f9abcc66ac9e5b94f4f105a064403
Scott Nichols <nicholss@google.com>
2019-02-28 11:30:04 -0800

    Update cloudevent sdk to include auto-generated uuid options.

--- cmd/heartbeats_receiver/main.go
+++ cmd/heartbeats_receiver/main.go
@@ -37,7 +37,11 @@ func receive(event cloudevents.Event) {
 		fmt.Printf("got data error: %s\n", err.Error())
 	}
 	log.Printf("CloudEvent:\n%s", event)
+<<<<<<< HEAD
 	log.Printf("[%s] %s %s: ", ec.Time, event.DataContentType(), ec.Source.String())
+=======
+	log.Printf("[%s] %s %s: ", ec.Time, ec.ContentType, ec.Source.String())
+>>>>>>> Update cloudevent sdk to include auto-generated uuid options.
 	log.Printf("\t%d, %q", hb.Sequence, hb.Label)
 }
 
