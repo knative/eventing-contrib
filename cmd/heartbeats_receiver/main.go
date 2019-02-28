bce4b10ce68c4259578eb8cb70d1156bf3da3114
Scott Nichols <nicholss@google.com>
2019-02-28 12:48:18 -0800

    fix minor fmt error.

--- cmd/heartbeats_receiver/main.go
+++ cmd/heartbeats_receiver/main.go
@@ -37,11 +37,7 @@ func receive(event cloudevents.Event) {
 		fmt.Printf("got data error: %s\n", err.Error())
 	}
 	log.Printf("CloudEvent:\n%s", event)
-<<<<<<< HEAD
 	log.Printf("[%s] %s %s: ", ec.Time, event.DataContentType(), ec.Source.String())
-=======
-	log.Printf("[%s] %s %s: ", ec.Time, ec.ContentType, ec.Source.String())
->>>>>>> Update cloudevent sdk to include auto-generated uuid options.
 	log.Printf("\t%d, %q", hb.Sequence, hb.Label)
 }
 
