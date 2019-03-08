f74c4b40065428a3fdf84e318b69c76303edcbd6
Adriano Cunha <35786489+adrcunha@users.noreply.github.com>
2019-03-08 09:43:41 -0800

    Update test-infra (#238)
    Get the latest updates for automatic releases.

--- vendor/github.com/knative/test-infra/scripts/release.sh
+++ vendor/github.com/knative/test-infra/scripts/release.sh
@@ -31,18 +31,6 @@ function banner() {
     make_banner "@" "$1"
 }
 
-# Capitalize the first letter of each word.
-# Parameters: $1..$n - words to capitalize.
-function capitalize() {
-  local words=("$1")
-  local capitalized=()
-  for word in $@; do
-    local initial="$(echo ${word:0:1}| tr 'a-z' 'A-Z')"
-    capitalized+=("${initial}${word:1}")
-  done
-  echo "${capitalized[@]}"
-}
-
 # Tag images in the yaml files if $TAG is not empty.
 # $KO_DOCKER_REPO is the registry containing the images to tag with $TAG.
 # Parameters: $1..$n - yaml files to parse for images.
@@ -142,28 +130,30 @@ function prepare_auto_release() {
   TAG_RELEASE=1
   PUBLISH_RELEASE=1
 
+  git fetch --all
   local tags="$(git tag | cut -d 'v' -f2 | cut -d '.' -f1-2 | sort | uniq)"
-  local branches="$( { (git branch -r | grep origin/release-) ; (git branch  | grep release-); } | cut -d '-' -f2 | sort | uniq)"
+  local branches="$( { (git branch -r | grep upstream/release-) ; (git branch | grep release-); } | cut -d '-' -f2 | sort | uniq)"
   RELEASE_VERSION=""
 
   [[ -n "${tags}" ]] || abort "cannot obtain release tags for the repository"
   [[ -n "${branches}" ]] || abort "cannot obtain release branches for the repository"
 
   for i in $branches; do
-    RELEASE_VERSION=$i
+    RELEASE_NUMBER=$i
     for j in $tags; do
       if [[ "$i" == "$j" ]]; then
-        RELEASE_VERSION=""
+        RELEASE_NUMBER=""
       fi
     done
   done
 
-  if [ -z "$RELEASE_VERSION" ]; then
+  if [ -z "$RELEASE_NUMBER" ]; then
     echo "*** No new release will be generated, as no new branches exist"
     exit  0
   fi
 
-  RELEASE_BRANCH="release-${RELEASE_VERSION}"
+  RELEASE_VERSION="${RELEASE_NUMBER}.0"
+  RELEASE_BRANCH="release-${RELEASE_NUMBER}"
   echo "Will create release ${RELEASE_VERSION} from branch ${RELEASE_BRANCH}"
   # If --release-notes not used, add a placeholder
   if [[ -z "${RELEASE_NOTES}" ]]; then
@@ -386,7 +376,11 @@ function main() {
 
   run_validation_tests ${VALIDATION_TESTS}
   banner "Building the release"
-  build_release || abort "error building the release"
+  build_release
+  # Do not use `||` above or any error will be swallowed.
+  if [[ $? -ne 0 ]]; then
+    abort "error building the release"
+  fi
   [[ -z "${YAMLS_TO_PUBLISH}" ]] && abort "no manifests were generated"
   echo "New release built successfully"
   if (( PUBLISH_RELEASE )); then
@@ -401,7 +395,7 @@ function main() {
 # Parameters: $1..$n - YAML files to add to the release.
 function publish_to_github() {
   (( PUBLISH_TO_GITHUB )) || return 0
-  local title="Knative $(capitalize ${REPO_NAME//-/ }) release ${TAG}"
+  local title="${REPO_NAME_FORMATTED} release ${TAG}"
   local attachments=()
   local description="$(mktemp)"
   local attachments_dir="$(mktemp -d)"
