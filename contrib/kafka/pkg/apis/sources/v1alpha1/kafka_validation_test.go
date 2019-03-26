dubee/eventing-sources

Kafka event source

contrib/kafka/pkg/apis/sources/v1alpha1/kafka_validation_test.go
@@ -1,0 +1,162 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+var (
+	fullSpec = KafkaSourceSpec{
+		Brokers:       "brokers",
+		Topic:         "topic",
+		ConsumerGroup: "group",
+		Sink: &corev1.ObjectReference{
+			APIVersion: "foo",
+			Kind:       "bar",
+			Namespace:  "baz",
+			Name:       "qux",
+		},
+		ServiceAccountName: "service-account-name",
+	}
+)
+
+func TestKafkaSourceCheckImmutableFields(t *testing.T) {
+	testCases := map[string]struct {
+		orig    *KafkaSourceSpec
+		updated KafkaSourceSpec
+		allowed bool
+	}{
+		"nil orig": {
+			updated: fullSpec,
+			allowed: true,
+		},
+		"Topic changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topic:              "some-other-topic",
+				Sink:               fullSpec.Sink,
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"Brokers changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topic:              "broker1,broker2",
+				Sink:               fullSpec.Sink,
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"Sink.APIVersion changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topic: fullSpec.Topic,
+				Sink: &corev1.ObjectReference{
+					APIVersion: "some-other-api-version",
+					Kind:       fullSpec.Sink.Kind,
+					Namespace:  fullSpec.Sink.Namespace,
+					Name:       fullSpec.Sink.Name,
+				},
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"Sink.Kind changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topic: fullSpec.Topic,
+				Sink: &corev1.ObjectReference{
+					APIVersion: fullSpec.Sink.APIVersion,
+					Kind:       "some-other-kind",
+					Namespace:  fullSpec.Sink.Namespace,
+					Name:       fullSpec.Sink.Name,
+				},
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"Sink.Namespace changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topic: fullSpec.Topic,
+				Sink: &corev1.ObjectReference{
+					APIVersion: fullSpec.Sink.APIVersion,
+					Kind:       fullSpec.Sink.Kind,
+					Namespace:  "some-other-namespace",
+					Name:       fullSpec.Sink.Name,
+				},
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"Sink.Name changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topic: fullSpec.Topic,
+				Sink: &corev1.ObjectReference{
+					APIVersion: fullSpec.Sink.APIVersion,
+					Kind:       fullSpec.Sink.Kind,
+					Namespace:  fullSpec.Sink.Namespace,
+					Name:       "some-other-name",
+				},
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"ServiceAccountName changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topic: fullSpec.Topic,
+				Sink: &corev1.ObjectReference{
+					APIVersion: fullSpec.Sink.APIVersion,
+					Kind:       fullSpec.Sink.Kind,
+					Namespace:  fullSpec.Sink.Namespace,
+					Name:       "some-other-name",
+				},
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"no change": {
+			orig:    &fullSpec,
+			updated: fullSpec,
+			allowed: true,
+		},
+	}
+
+	for n, tc := range testCases {
+		t.Run(n, func(t *testing.T) {
+			var orig *KafkaSource
+			if tc.orig != nil {
+				orig = &KafkaSource{
+					Spec: *tc.orig,
+				}
+			}
+			updated := &KafkaSource{
+				Spec: tc.updated,
+			}
+			err := updated.CheckImmutableFields(orig)
+			if tc.allowed != (err == nil) {
+				t.Fatalf("Unexpected immutable field check. Expected %v. Actual %v", tc.allowed, err)
+			}
+		})
+	}
+}

contrib/kafka/pkg/reconciler/resources/receive_adapter.go
@@ -1,0 +1,102 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resources
+
+import (
+	"fmt"
+	"github.com/knative/eventing-sources/contrib/kafka/pkg/apis/sources/v1alpha1"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"strconv"
+)
+
+type ReceiveAdapterArgs struct {
+	Image   string
+	Source  *v1alpha1.KafkaSource
+	Labels  map[string]string
+	SinkURI string
+}
+
+func MakeReceiveAdapter(args *ReceiveAdapterArgs) *v1.Deployment {
+	replicas := int32(1)
+	return &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace:    args.Source.Namespace,
+			GenerateName: fmt.Sprintf("%s-", args.Source.Name),
+			Labels:       args.Labels,
+		},
+		Spec: v1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: args.Labels,
+			},
+			Replicas: &replicas,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"sidecar.istio.io/inject": "true",
+					},
+					Labels: args.Labels,
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: args.Source.Spec.ServiceAccountName,
+					Containers: []corev1.Container{
+						{
+							Name:            "receive-adapter",
+							Image:           args.Image,
+							ImagePullPolicy: "Always",
+							Env: []corev1.EnvVar{
+								{
+									Name:  "KAFKA_BROKERS",
+									Value: args.Source.Spec.Brokers,
+								},
+								{
+									Name:  "KAFKA_TOPIC",
+									Value: args.Source.Spec.Topic,
+								},
+								{
+									Name:  "KAFKA_CONSUMER_GROUP",
+									Value: args.Source.Spec.ConsumerGroup,
+								},
+								{
+									Name:  "KAFKA_NET_SASL_ENABLE",
+									Value: strconv.FormatBool(args.Source.Spec.Net.SASL.Enable),
+								},
+								{
+									Name:  "KAFKA_NET_SASL_USER",
+									Value: args.Source.Spec.Net.SASL.User,
+								},
+								{
+									Name:  "KAFKA_NET_SASL_PASSWORD",
+									Value: args.Source.Spec.Net.SASL.Password,
+								},
+								{
+									Name:  "KAFKA_NET_TLS_ENABLE",
+									Value: strconv.FormatBool(args.Source.Spec.Net.TLS.Enable),
+								},
+								{
+									Name:  "SINK_URI",
+									Value: args.SinkURI,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}

contrib/kafka/pkg/reconciler/resources/receive_adapter_test.go
@@ -1,0 +1,141 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resources
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"github.com/knative/eventing-sources/contrib/kafka/pkg/apis/sources/v1alpha1"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+)
+
+func TestMakeReceiveAdapter(t *testing.T) {
+	src := &v1alpha1.KafkaSource{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "source-name",
+			Namespace: "source-namespace",
+		},
+		Spec: v1alpha1.KafkaSourceSpec{
+			ServiceAccountName: "source-svc-acct",
+			Topic:              "topic",
+			Brokers:            "broker1,broker2",
+			ConsumerGroup:      "group",
+			Net: v1alpha1.KafkaSourceNetSpec{
+				SASL: v1alpha1.KafkaSourceSASLSpec{
+					Enable:   true,
+					User:     "user",
+					Password: "password",
+				},
+				TLS: v1alpha1.KafkaSourceTLSSpec{
+					Enable: true,
+				},
+			},
+		},
+	}
+
+	got := MakeReceiveAdapter(&ReceiveAdapterArgs{
+		Image:  "test-image",
+		Source: src,
+		Labels: map[string]string{
+			"test-key1": "test-value1",
+			"test-key2": "test-value2",
+		},
+		SinkURI: "sink-uri",
+	})
+
+	one := int32(1)
+	want := &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace:    "source-namespace",
+			GenerateName: "source-name-",
+			Labels: map[string]string{
+				"test-key1": "test-value1",
+				"test-key2": "test-value2",
+			},
+		},
+		Spec: v1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"test-key1": "test-value1",
+					"test-key2": "test-value2",
+				},
+			},
+			Replicas: &one,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"sidecar.istio.io/inject": "true",
+					},
+					Labels: map[string]string{
+						"test-key1": "test-value1",
+						"test-key2": "test-value2",
+					},
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: "source-svc-acct",
+					Containers: []corev1.Container{
+						{
+							Name:            "receive-adapter",
+							Image:           "test-image",
+							ImagePullPolicy: "Always",
+							Env: []corev1.EnvVar{
+								{
+									Name:  "KAFKA_BROKERS",
+									Value: "broker1,broker2",
+								},
+								{
+									Name:  "KAFKA_TOPIC",
+									Value: "topic",
+								},
+								{
+									Name:  "KAFKA_CONSUMER_GROUP",
+									Value: "group",
+								},
+								{
+									Name:  "KAFKA_NET_SASL_ENABLE",
+									Value: "true",
+								},
+								{
+									Name:  "KAFKA_NET_SASL_USER",
+									Value: "user",
+								},
+								{
+									Name:  "KAFKA_NET_SASL_PASSWORD",
+									Value: "password",
+								},
+								{
+									Name:  "KAFKA_NET_TLS_ENABLE",
+									Value: "true",
+								},
+								{
+									Name:  "SINK_URI",
+									Value: "sink-uri",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("unexpected deploy (-want, +got) = %v", diff)
+	}
+}

knative/eventing-contrib

Co-authored-by: Matthias Wessendorf <mwessend@redhat.com>

contrib/kafka/pkg/apis/sources/v1alpha1/kafka_validation_test.go
@@ -1,0 +1,162 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+var (
+	fullSpec = KafkaSourceSpec{
+		Brokers:       "brokers",
+		Topics:        "topic",
+		ConsumerGroup: "group",
+		Sink: &corev1.ObjectReference{
+			APIVersion: "foo",
+			Kind:       "bar",
+			Namespace:  "baz",
+			Name:       "qux",
+		},
+		ServiceAccountName: "service-account-name",
+	}
+)
+
+func TestKafkaSourceCheckImmutableFields(t *testing.T) {
+	testCases := map[string]struct {
+		orig    *KafkaSourceSpec
+		updated KafkaSourceSpec
+		allowed bool
+	}{
+		"nil orig": {
+			updated: fullSpec,
+			allowed: true,
+		},
+		"Topic changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topics:             "some-other-topic",
+				Sink:               fullSpec.Sink,
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"Brokers changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topics:             "broker1,broker2",
+				Sink:               fullSpec.Sink,
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"Sink.APIVersion changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topics: fullSpec.Topics,
+				Sink: &corev1.ObjectReference{
+					APIVersion: "some-other-api-version",
+					Kind:       fullSpec.Sink.Kind,
+					Namespace:  fullSpec.Sink.Namespace,
+					Name:       fullSpec.Sink.Name,
+				},
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"Sink.Kind changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topics: fullSpec.Topics,
+				Sink: &corev1.ObjectReference{
+					APIVersion: fullSpec.Sink.APIVersion,
+					Kind:       "some-other-kind",
+					Namespace:  fullSpec.Sink.Namespace,
+					Name:       fullSpec.Sink.Name,
+				},
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"Sink.Namespace changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topics: fullSpec.Topics,
+				Sink: &corev1.ObjectReference{
+					APIVersion: fullSpec.Sink.APIVersion,
+					Kind:       fullSpec.Sink.Kind,
+					Namespace:  "some-other-namespace",
+					Name:       fullSpec.Sink.Name,
+				},
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"Sink.Name changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topics: fullSpec.Topics,
+				Sink: &corev1.ObjectReference{
+					APIVersion: fullSpec.Sink.APIVersion,
+					Kind:       fullSpec.Sink.Kind,
+					Namespace:  fullSpec.Sink.Namespace,
+					Name:       "some-other-name",
+				},
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"ServiceAccountName changed": {
+			orig: &fullSpec,
+			updated: KafkaSourceSpec{
+				Topics: fullSpec.Topics,
+				Sink: &corev1.ObjectReference{
+					APIVersion: fullSpec.Sink.APIVersion,
+					Kind:       fullSpec.Sink.Kind,
+					Namespace:  fullSpec.Sink.Namespace,
+					Name:       "some-other-name",
+				},
+				ServiceAccountName: fullSpec.ServiceAccountName,
+			},
+			allowed: false,
+		},
+		"no change": {
+			orig:    &fullSpec,
+			updated: fullSpec,
+			allowed: true,
+		},
+	}
+
+	for n, tc := range testCases {
+		t.Run(n, func(t *testing.T) {
+			var orig *KafkaSource
+			if tc.orig != nil {
+				orig = &KafkaSource{
+					Spec: *tc.orig,
+				}
+			}
+			updated := &KafkaSource{
+				Spec: tc.updated,
+			}
+			err := updated.CheckImmutableFields(orig)
+			if tc.allowed != (err == nil) {
+				t.Fatalf("Unexpected immutable field check. Expected %v. Actual %v", tc.allowed, err)
+			}
+		})
+	}
+}

contrib/kafka/pkg/reconciler/resources/receive_adapter.go
@@ -1,0 +1,102 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resources
+
+import (
+	"fmt"
+	"github.com/knative/eventing-sources/contrib/kafka/pkg/apis/sources/v1alpha1"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"strconv"
+)
+
+type ReceiveAdapterArgs struct {
+	Image   string
+	Source  *v1alpha1.KafkaSource
+	Labels  map[string]string
+	SinkURI string
+}
+
+func MakeReceiveAdapter(args *ReceiveAdapterArgs) *v1.Deployment {
+	replicas := int32(1)
+	return &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace:    args.Source.Namespace,
+			GenerateName: fmt.Sprintf("%s-", args.Source.Name),
+			Labels:       args.Labels,
+		},
+		Spec: v1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: args.Labels,
+			},
+			Replicas: &replicas,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"sidecar.istio.io/inject": "true",
+					},
+					Labels: args.Labels,
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: args.Source.Spec.ServiceAccountName,
+					Containers: []corev1.Container{
+						{
+							Name:            "receive-adapter",
+							Image:           args.Image,
+							ImagePullPolicy: "Always",
+							Env: []corev1.EnvVar{
+								{
+									Name:  "KAFKA_BROKERS",
+									Value: args.Source.Spec.Brokers,
+								},
+								{
+									Name:  "KAFKA_TOPICS",
+									Value: args.Source.Spec.Topics,
+								},
+								{
+									Name:  "KAFKA_CONSUMER_GROUP",
+									Value: args.Source.Spec.ConsumerGroup,
+								},
+								{
+									Name:  "KAFKA_NET_SASL_ENABLE",
+									Value: strconv.FormatBool(args.Source.Spec.Net.SASL.Enable),
+								},
+								{
+									Name:  "KAFKA_NET_SASL_USER",
+									Value: args.Source.Spec.Net.SASL.User,
+								},
+								{
+									Name:  "KAFKA_NET_SASL_PASSWORD",
+									Value: args.Source.Spec.Net.SASL.Password,
+								},
+								{
+									Name:  "KAFKA_NET_TLS_ENABLE",
+									Value: strconv.FormatBool(args.Source.Spec.Net.TLS.Enable),
+								},
+								{
+									Name:  "SINK_URI",
+									Value: args.SinkURI,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}

contrib/kafka/pkg/reconciler/resources/receive_adapter_test.go
@@ -1,0 +1,141 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resources
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"github.com/knative/eventing-sources/contrib/kafka/pkg/apis/sources/v1alpha1"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+)
+
+func TestMakeReceiveAdapter(t *testing.T) {
+	src := &v1alpha1.KafkaSource{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "source-name",
+			Namespace: "source-namespace",
+		},
+		Spec: v1alpha1.KafkaSourceSpec{
+			ServiceAccountName: "source-svc-acct",
+			Topics:             "topic1,topic2",
+			Brokers:            "broker1,broker2",
+			ConsumerGroup:      "group",
+			Net: v1alpha1.KafkaSourceNetSpec{
+				SASL: v1alpha1.KafkaSourceSASLSpec{
+					Enable:   true,
+					User:     "user",
+					Password: "password",
+				},
+				TLS: v1alpha1.KafkaSourceTLSSpec{
+					Enable: true,
+				},
+			},
+		},
+	}
+
+	got := MakeReceiveAdapter(&ReceiveAdapterArgs{
+		Image:  "test-image",
+		Source: src,
+		Labels: map[string]string{
+			"test-key1": "test-value1",
+			"test-key2": "test-value2",
+		},
+		SinkURI: "sink-uri",
+	})
+
+	one := int32(1)
+	want := &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace:    "source-namespace",
+			GenerateName: "source-name-",
+			Labels: map[string]string{
+				"test-key1": "test-value1",
+				"test-key2": "test-value2",
+			},
+		},
+		Spec: v1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"test-key1": "test-value1",
+					"test-key2": "test-value2",
+				},
+			},
+			Replicas: &one,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"sidecar.istio.io/inject": "true",
+					},
+					Labels: map[string]string{
+						"test-key1": "test-value1",
+						"test-key2": "test-value2",
+					},
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: "source-svc-acct",
+					Containers: []corev1.Container{
+						{
+							Name:            "receive-adapter",
+							Image:           "test-image",
+							ImagePullPolicy: "Always",
+							Env: []corev1.EnvVar{
+								{
+									Name:  "KAFKA_BROKERS",
+									Value: "broker1,broker2",
+								},
+								{
+									Name:  "KAFKA_TOPICS",
+									Value: "topic1,topic2",
+								},
+								{
+									Name:  "KAFKA_CONSUMER_GROUP",
+									Value: "group",
+								},
+								{
+									Name:  "KAFKA_NET_SASL_ENABLE",
+									Value: "true",
+								},
+								{
+									Name:  "KAFKA_NET_SASL_USER",
+									Value: "user",
+								},
+								{
+									Name:  "KAFKA_NET_SASL_PASSWORD",
+									Value: "password",
+								},
+								{
+									Name:  "KAFKA_NET_TLS_ENABLE",
+									Value: "true",
+								},
+								{
+									Name:  "SINK_URI",
+									Value: "sink-uri",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("unexpected deploy (-want, +got) = %v", diff)
+	}
+}