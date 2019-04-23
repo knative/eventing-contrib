c350570178a55c71e596704560bb317975f51359
Lionel Villard <villard@us.ibm.com>
2019-04-23 18:11:29 -0400

    Add new Kubernetes apiserver event source (#294)

---
+++ contrib/apiserver/pkg/adapter/adapter.go
@@ -0,0 +1,213 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package adapter
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/cloudevents/sdk-go/pkg/cloudevents"
+	eventsclient "github.com/cloudevents/sdk-go/pkg/cloudevents/client"
+	"github.com/cloudevents/sdk-go/pkg/cloudevents/types"
+	"github.com/knative/eventing-sources/pkg/kncloudevents"
+	"github.com/knative/pkg/logging"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	controllerAgentName = "resource-controller-adapter"
+	updateEventType     = "dev.knative.apiserver.object.update"
+	deleteEventType     = "dev.knative.apiserver.object.delete"
+
+	envSinkURI    = "SINK_URI"
+	envAPIVersion = "APIVERSION_"
+	envKind       = "KIND_"
+	envOwnerRef   = "OWNEREF_" // format is OWNEREF_<RESOURCEINDEX>_[APIVERSION|KIND]_<OWNEREFINDEX>
+)
+
+// Add creates new ApiServerSource controllers, one per resource,
+// and adds them to the manager.
+func Add(mgr manager.Manager) error {
+	sinkURI, defined := os.LookupEnv(envSinkURI)
+	if !defined {
+		return fmt.Errorf("required environment variable not defined '%s'", envSinkURI)
+	}
+
+	eventsClient, err := kncloudevents.NewDefaultClient(sinkURI)
+	if err != nil {
+		return err
+	}
+
+	// Create one controller per watch
+	i := 0
+	for {
+		stri := strconv.Itoa(i)
+
+		apiVersion, defined := os.LookupEnv(envAPIVersion + stri)
+		if !defined {
+			break
+		}
+
+		kind, defined := os.LookupEnv(envKind + stri)
+		if !defined {
+			return fmt.Errorf("required environment variable not defined '%s'", envKind+stri)
+		}
+
+		gv, err := schema.ParseGroupVersion(apiVersion)
+		if err != nil {
+			return err
+		}
+
+		dependents := make([]source.Kind, 2)
+		j := 0
+		for {
+			strj := strconv.Itoa(j)
+
+			ownerRefAPIVersion, defined := os.LookupEnv(envOwnerRef + stri + "_" + envAPIVersion + strj)
+			if !defined {
+				break
+			}
+
+			ownerRefKind, defined := os.LookupEnv(envOwnerRef + stri + "_" + envKind + strj)
+			if !defined {
+				return fmt.Errorf("required environment variable not defined '%s'", envOwnerRef+stri+"_"+envKind+strj)
+			}
+
+			ownerRefGV, err := schema.ParseGroupVersion(ownerRefAPIVersion)
+			if err != nil {
+				return err
+			}
+
+			u := &unstructured.Unstructured{}
+			u.SetGroupVersionKind(schema.GroupVersionKind{Kind: ownerRefKind, Group: ownerRefGV.Group, Version: ownerRefGV.Version})
+
+			dependents = append(dependents, source.Kind{Type: u})
+		}
+
+		// don't use the sdk here as we are using unstructured watches
+		err = add(mgr, &reconciler{
+			client:       mgr.GetClient(),
+			eventsClient: eventsClient,
+			gvk:          schema.GroupVersionKind{Kind: kind, Group: gv.Group, Version: gv.Version},
+		}, dependents)
+
+		if err != nil {
+			return err
+		}
+
+		i++
+	}
+	return nil
+}
+
+// Add creates a controller for esource-controller-adapter.
+func add(mgr manager.Manager, r *reconciler, dependents []source.Kind) error {
+	// Setup a new controller to Reconcile Subscriptions.
+	c, err := controller.New(controllerAgentName, mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch Parent events and enqueue Parent object key.
+	u := &unstructured.Unstructured{}
+	u.SetGroupVersionKind(r.gvk)
+
+	err = c.Watch(&source.Kind{Type: u}, &handler.EnqueueRequestForObject{})
+
+	// Add watches for dependents
+	for _, dependent := range dependents {
+		err = c.Watch(&dependent, &handler.EnqueueRequestForOwner{
+			IsController: true,
+			OwnerType:    u,
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// reconciler reconciles a ApiServerSource object
+type reconciler struct {
+	client       client.Client
+	eventsClient eventsclient.Client
+	gvk          schema.GroupVersionKind
+}
+
+func (r *reconciler) InjectClient(c client.Client) error {
+	r.client = c
+	return nil
+}
+
+func (r *reconciler) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	ctx := context.TODO()
+	logger := logging.FromContext(ctx)
+
+	object := &unstructured.Unstructured{}
+	object.SetGroupVersionKind(r.gvk)
+	err := r.client.Get(ctx, request.NamespacedName, object)
+
+	if errors.IsNotFound(err) {
+		logger.Errorf("could not find %s %v\n", request.Name, request)
+		return reconcile.Result{}, nil
+	}
+	eventType := updateEventType
+	timestamp := object.GetCreationTimestamp()
+	if object.GetDeletionTimestamp() != nil {
+		eventType = deleteEventType
+		timestamp = *object.GetDeletionTimestamp()
+	}
+
+	objectRef := corev1.ObjectReference{
+		APIVersion: object.GetAPIVersion(),
+		Kind:       object.GetKind(),
+		Name:       object.GetName(),
+		Namespace:  object.GetNamespace(),
+	}
+
+	event := cloudevents.Event{
+		Context: cloudevents.EventContextV02{
+			ID:     string(object.GetUID()),
+			Type:   eventType,
+			Source: *types.ParseURLRef(object.GetSelfLink()),
+			Time:   &types.Timestamp{Time: timestamp.Time},
+		}.AsV02(),
+		Data: objectRef,
+	}
+
+	if _, err := r.eventsClient.Send(ctx, event); err != nil {
+		logger.Error("failed to send cloudevent (retrying)", err)
+
+		// Retrying...
+		return reconcile.Result{Requeue: true}, nil
+	}
+
+	return reconcile.Result{}, nil
+}
---
+++ contrib/apiserver/pkg/adapter/adapter_test.go
@@ -0,0 +1,175 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package adapter
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/cloudevents/sdk-go/pkg/cloudevents/datacodec/json"
+	"github.com/google/go-cmp/cmp"
+	controllertesting "github.com/knative/eventing-sources/pkg/controller/testing"
+	"github.com/knative/eventing-sources/pkg/kncloudevents"
+	"go.uber.org/zap"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+const (
+	sourceName = "test-apiserver-adapter"
+	sourceUID  = "1234-5678-90"
+	testNS     = "testnamespace"
+)
+
+type testCase struct {
+	controllertesting.TestCase
+
+	// Where to send events
+	sink func(http.ResponseWriter, *http.Request)
+
+	// Expected event data
+	data interface{}
+}
+
+func TestReconcile(t *testing.T) {
+	testCases := []testCase{
+		testCase{
+			TestCase: controllertesting.TestCase{
+				Name: "Receive Pod creation event",
+				InitialState: []runtime.Object{
+					getPod(),
+				},
+				Reconciles: getPod(),
+			},
+			sink: sinkAccepted,
+			data: decode(t, encode(t, getPodRef())),
+		},
+	}
+
+	request := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: testNS,
+			Name:      sourceName,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			// Create fake sink server
+			h := &fakeHandler{
+				handler: tc.sink,
+			}
+
+			sinkServer := httptest.NewServer(h)
+			defer sinkServer.Close()
+
+			// Bind cloud event client
+			ceClient, err := kncloudevents.NewDefaultClient(sinkServer.URL)
+			if err != nil {
+				t.Errorf("cannot create cloud event client: %v", zap.Error(err))
+			}
+
+			// Create fake k8s client
+			c := fake.NewFakeClient(tc.InitialState...)
+
+			r := &reconciler{
+				client:       c,
+				eventsClient: ceClient,
+				gvk:          tc.Reconciles.GetObjectKind().GroupVersionKind(),
+			}
+
+			_, err = r.Reconcile(request)
+			if err != nil {
+				t.Errorf("Expected no error")
+			}
+
+			if diff := cmp.Diff(tc.data, decode(t, h.body)); diff != "" {
+				t.Errorf("incorrect event (-want, +got): %v", diff)
+			}
+		})
+	}
+}
+
+func getPod() runtime.Object {
+	return &v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Pod",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      sourceName,
+			Namespace: testNS,
+			SelfLink:  "/apis/v1/namespaces/" + testNS + "/pod/" + sourceName,
+		},
+	}
+}
+
+func getPodRef() corev1.ObjectReference {
+	return corev1.ObjectReference{
+		APIVersion: "v1",
+		Kind:       "Pod",
+		Name:       sourceName,
+		Namespace:  testNS,
+	}
+}
+
+type fakeHandler struct {
+	body   []byte
+	header http.Header
+
+	handler func(http.ResponseWriter, *http.Request)
+}
+
+func (h *fakeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.header = r.Header
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "can not read body", http.StatusBadRequest)
+		return
+	}
+	h.body = body
+	defer r.Body.Close()
+	h.handler(w, r)
+}
+
+func sinkAccepted(writer http.ResponseWriter, req *http.Request) {
+	writer.WriteHeader(http.StatusOK)
+}
+
+func encode(t *testing.T, data interface{}) string {
+	b, err := json.Encode(data)
+	if err != nil {
+		t.Fatalf("failed to encode data: %v", err)
+	}
+	return string(b)
+}
+
+func decode(t *testing.T, data interface{}) interface{} {
+	var out interface{}
+	err := json.Decode(data, &out)
+	if err != nil {
+		t.Fatalf("failed to decode data: %v", err)
+	}
+	return out
+}
