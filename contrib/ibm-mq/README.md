432ab41a5c896647404b69a1ea799c91c7c95d2e
tzununbekov <t.zununbekov@gmail.com>
2019-07-31 20:14:27 +0600

    "ko apply"-able ibm-mq containersource

---
+++ contrib/ibm-mq/README.md
@@ -0,0 +1,108 @@
+## IBM MQ source
+
+This event source is meant to be used as a Container Source within a Knative cluster in order to consume messages from IBM Message Queue.
+
+### Local Build and Usage
+
+1. Setup a local IBM MQ with the following commands:
+
+```shell
+docker volume create qmdata
+docker network create mqnetwork
+docker run --env LICENSE=accept \
+           --env MQ_QMGR_NAME=QM1 \
+           --volume qmdata:/mnt/mqm \
+           --publish 1414:1414 \
+           --publish 9443:9443 \
+           --network mqnetwork \
+           --network-alias qmgr \
+           --detach \
+           --env MQ_APP_PASSWORD=password \
+           --name mq \
+           ibmcom/mq:latest
+```
+
+If you face any issues please follow the official [tutorial](https://developer.ibm.com/messaging/learn-mq/mq-tutorials/mq-connect-to-queue-manager/#docker)
+
+1. Connect the Knative event display:
+
+```shell
+docker run --name display --net=container:mq -d gcr.io/knative-releases/github.com/knative/eventing-sources/cmd/event_display@sha256:37ace92b63fc516ad4c8331b6b3b2d84e4ab2d8ba898e387c0b6f68f0e3081c4
+```
+
+1. To build the container image from the local source code
+
+```shell
+docker build -t mqsource .
+```
+
+1. Run this _source_ locally with:
+
+```shell
+docker run --rm -e PASSWORD=password \
+                --net=container:mq \
+                mqsource --sink http://localhost:8080
+```
+
+1. Open the MQ console:
+
+Using _Admin_ and _passw0rd_ as default development credentials do:
+
+`open https://localhost:9443/ibmmq/console/` 
+
+Add messages to `DEV.QUEUE.1` queue and check the logs of the Knative event display to see the CloudEvents being received. They will look something like
+
+```
+$ docker logs display
+☁️  cloudevents.Event
+Validation: valid
+Context Attributes,
+  cloudEventsVersion: 0.1
+  eventType: message queue item
+  source: ibm:mq
+  eventID: 08e05aa7-c9cc-4139-a041-9623cb101613
+  eventTime: 2019-06-07T13:42:10.6321831Z
+  contentType: application/json
+Data,
+  {
+    "message_descriptor": {
+      "Version": 1,
+      "Report": 0,
+      "MsgType": 8,
+      "Expiry": -1,
+      "Feedback": 0,
+      "Encoding": 273,
+      "CodedCharSetId": 1208,
+      "Format": "MQSTR",
+      "Priority": 0,
+      "Persistence": 0,
+      "MsgId": "QU1RIFFNMSAgICAgICAgIGCQ/1wCdo8g",
+      "CorrelId": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+      "BackoutCount": 0,
+      "ReplyToQ": "",
+      "ReplyToQMgr": "QM1",
+      "UserIdentifier": "mqm",
+      "AccountingToken": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+      "ApplIdentityData": "admin",
+      "PutApplType": 7,
+      "PutApplName": "IBM MQ Web Admin/REST API",
+      "PutDate": "20190611",
+      "PutTime": "11444037",
+      "ApplOriginData": "",
+      "GroupId": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+      "MsgSeqNumber": 1,
+      "Offset": 0,
+      "MsgFlags": 0,
+      "OriginalLength": -1
+    },
+    "message_data": "Hello Knative"
+  }
+```
+
+### Knative usage
+
+Edit the Container source manifest to specify your queue and connection parameters then and apply it like so:
+
+```
+kubectl apply -f mq-source.yaml
+```
---
+++ contrib/ibm-mq/main.go
@@ -0,0 +1,182 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/caarlos0/env"
+	"github.com/ibm-messaging/mq-golang/ibmmq"
+	"knative.dev/pkg/cloudevents"
+)
+
+var sink string
+var wg sync.WaitGroup
+
+type config struct {
+	QueueManager   string `env:"QUEUE_MANAGER" envDefault:"QM1"`
+	ChannelName    string `env:"CHANNEL_NAME" envDefault:"DEV.APP.SVRCONN"`
+	ConnectionName string `env:"CONNECTION_NAME" envDefault:"localhost(1414)"`
+	UserID         string `env:"USER_ID" envDefault:"app"`
+	Password       string `env:"PASSWORD" envDefault:"password"`
+	QueueName      string `env:"QUEUE_NAME" envDefault:"DEV.QUEUE.1"`
+}
+
+//MQMessage wraps message descriptor and message body into one struct
+type MQMessage struct {
+	Message     *ibmmq.MQMD `json:"message_descriptor"`
+	MessageData string      `json:"message_data"`
+}
+
+func init() {
+	flag.StringVar(&sink, "sink", "http://localhost:8080", "where to sink events to")
+}
+
+func main() {
+	flag.Parse()
+
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	cloudEventsClient := cloudevents.NewClient(
+		sink,
+		cloudevents.Builder{
+			Source:    "ibm:mq",
+			EventType: "message queue item",
+		},
+	)
+
+	fmt.Printf("%+v\n", cfg)
+
+	// create IBM MQ channel definition
+	channelDefinition := ibmmq.NewMQCD()
+	channelDefinition.ChannelName = cfg.ChannelName
+	channelDefinition.ConnectionName = cfg.ConnectionName
+
+	// init connection security params
+	connSecParams := ibmmq.NewMQCSP()
+	connSecParams.AuthenticationType = ibmmq.MQCSP_AUTH_USER_ID_AND_PWD
+	connSecParams.UserId = cfg.UserID
+	connSecParams.Password = cfg.Password
+
+	// setup MQ connection params
+	connOptions := ibmmq.NewMQCNO()
+	connOptions.Options = ibmmq.MQCNO_CLIENT_BINDING
+	// connOptions.ApplName = "Triggermesh eventing"
+	connOptions.ClientConn = channelDefinition
+	connOptions.SecurityParms = connSecParams
+
+	// And now we can try to connect. Wait a short time before disconnecting.
+	qMgrObject, err := ibmmq.Connx(cfg.QueueManager, connOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Connection to %s succeeded.\n", cfg.QueueManager)
+	defer disconnect(qMgrObject)
+
+	// Create the Object Descriptor that allows us to give the queue name
+	mqod := ibmmq.NewMQOD()
+
+	// We have to say how we are going to use this queue. In this case, to GET
+	// messages. That is done in the openOptions parameter.
+	openOptions := ibmmq.MQOO_INPUT_EXCLUSIVE
+
+	// Opening a QUEUE (rather than a Topic or other object type) and give the name
+	mqod.ObjectType = ibmmq.MQOT_Q
+	mqod.ObjectName = cfg.QueueName
+
+	qObject, err := qMgrObject.Open(mqod, openOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Opened queue", qObject.Name)
+	defer close(qObject)
+	defer wg.Wait()
+
+	for {
+		// The GET requires control structures, the Message Descriptor (MQMD)
+		// and Get Options (MQGMO). Create those with default values.
+		msgDescriptor := ibmmq.NewMQMD()
+		msgOptions := ibmmq.NewMQGMO()
+
+		// The default options are OK, but it's always
+		// a good idea to be explicit about transactional boundaries as
+		// not all platforms behave the same way.
+		msgOptions.Options = ibmmq.MQGMO_NO_SYNCPOINT
+
+		// Set options to wait for a maximum of 3 seconds for any new message to arrive
+		msgOptions.Options |= ibmmq.MQGMO_WAIT
+		msgOptions.WaitInterval = 3 * 1000 // The WaitInterval is in milliseconds
+
+		// Create a buffer for the message data. This one is large enough
+		// for the messages put by the amqsput sample.
+		buffer := make([]byte, 1024)
+
+		// Now we can try to get the message
+		_, err := qObject.Get(msgDescriptor, msgOptions, buffer)
+		if err != nil {
+			mqret := err.(*ibmmq.MQReturn)
+			if mqret.MQRC == ibmmq.MQRC_NO_MSG_AVAILABLE {
+				continue
+			}
+			break
+		}
+
+		wg.Add(1)
+		go func(md *ibmmq.MQMD, messageData []byte) {
+			defer wg.Done()
+			log.Printf("New message: %v", bytes.Trim(messageData, "\u0000"))
+			msg := MQMessage{
+				Message:     md,
+				MessageData: string(bytes.Trim(messageData, "\u0000")),
+			}
+			if err := sendMessage(cloudEventsClient, &msg); err != nil {
+				log.Printf("Failed to send message: %v\n", err)
+			}
+		}(msgDescriptor, buffer)
+	}
+}
+
+// Disconnect from the queue manager
+func disconnect(qMgrObject ibmmq.MQQueueManager) error {
+	err := qMgrObject.Disc()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return err
+}
+
+// Close the queue if it was opened
+func close(object ibmmq.MQObject) error {
+	err := object.Close(0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return err
+}
+
+func sendMessage(client *cloudevents.Client, message *MQMessage) error {
+	return client.Send(message)
+}
