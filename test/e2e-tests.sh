d9b2566e1a15eeb81cfd5dcccd2195d2229a02bd
Chi Zhang <chizhg@google.com>
2019-08-05 11:01:49 -0700

    experiment4

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -1,12 +1,13 @@
 source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/e2e-tests.sh
 
+function knative_setup() {
+  echo "current path: $(pwd)"
+  echo "GOPATH: ${GOPATH}"
+
+  cd ${GOPATH} && mkdir -p src/github.com && cd src/github.com
+  git clone https://github.com/knative/eventing
+  cd knative/eventing
+  ko apply -f config/
+}
+
 initialize $@ --skip-istio-addon
-
-echo "current path: $(pwd)"
-
-echo "GOPATH: ${GOPATH}"
-
-cd ${GOPATH} && mkdir -p src/github.com && cd src/github.com
-git clone https://github.com/knative/eventing
-cd knative/eventing
-ko apply -f config/
