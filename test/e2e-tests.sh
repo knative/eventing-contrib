2c159f058875c7db7f0e5d9adfe92ab2ac656bbc
Chi Zhang <chizhg@google.com>
2019-08-05 10:56:32 -0700

    experiment2

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -3,3 +3,8 @@ source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/e2e-tests.sh
 echo "current path: $(pwd)"
 
 echo "GOPATH: ${GOPATH}"
+
+cd ${GOPATH} && mkdir -p src/github.com && cd src/github.com
+git clone https://github.com/knative/eventing
+cd knative/eventing
+ko apply -f config/
