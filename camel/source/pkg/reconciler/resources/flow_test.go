1f392612ecd8ce7255e69dcf44ecfa720d6e27e5
mattmoor-sockpuppet <mattmoor+sockpuppet@google.com>
2019-08-06 09:24:55 -0700

    golang format tools (#509)
    Produced via:
      `gofmt -s -w $(find -path './vendor' -prune -o -type f -name '*.go' -print))`
      `goimports -w $(find -name '*.go' | grep -v vendor)`
    /assign @n3wscott

--- camel/source/pkg/reconciler/resources/flow_test.go
+++ camel/source/pkg/reconciler/resources/flow_test.go
@@ -17,8 +17,9 @@ limitations under the License.
 package resources
 
 import (
-	"github.com/google/go-cmp/cmp"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestCamelFlow(t *testing.T) {
