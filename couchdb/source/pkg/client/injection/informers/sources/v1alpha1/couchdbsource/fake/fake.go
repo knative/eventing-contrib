c66ff1a8020f763c128bf74003e1549404ddb073
Lionel Villard <villard@us.ibm.com>
2019-09-23 09:01:25 -0400

    update dependencies

--- couchdb/source/pkg/client/injection/informers/sources/v1alpha1/couchdbsource/fake/fake.go
+++ couchdb/source/pkg/client/injection/informers/sources/v1alpha1/couchdbsource/fake/fake.go
@@ -21,7 +21,7 @@ package fake
 import (
 	"context"
 
-	fake "knative.dev/eventing-contrib/couchdb/source/pkg/client/injection/informers/sources/factory/fake"
+	fake "knative.dev/eventing-contrib/couchdb/source/pkg/client/injection/informers/factory/fake"
 	couchdbsource "knative.dev/eventing-contrib/couchdb/source/pkg/client/injection/informers/sources/v1alpha1/couchdbsource"
 	controller "knative.dev/pkg/controller"
 	injection "knative.dev/pkg/injection"
