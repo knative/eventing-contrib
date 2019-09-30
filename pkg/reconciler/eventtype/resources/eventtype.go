00013a118382fc341f6f7050466c24a741365662
nachocano <nachoacano@gmail.com>
2019-09-30 14:57:30 -0700

    removing old duplicated stuff

--- pkg/reconciler/eventtype/resources/eventtype.go
+++ pkg/reconciler/eventtype/resources/eventtype.go
@@ -18,48 +18,21 @@ package resources
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/validation"
 	eventingv1alpha1 "knative.dev/eventing/pkg/apis/eventing/v1alpha1"
+	"knative.dev/eventing/pkg/utils"
 )
-
-const (
-	// Number of characters to keep available just in case the prefix used in generateName
-	// exceeds the maximum allowed for k8s names.
-	generateNameSafety = 10
-)
-
-// Only allow alphanumeric, '-' or '.'.
-var validChars = regexp.MustCompile(`[^-\.a-z0-9]+`)
 
 // MakeEventType creates the in-memory representation of the EventType in the specified namespace, with the given spec,
 // and label.
 func MakeEventType(spec eventingv1alpha1.EventTypeSpec, namespace string, lbl map[string]string) eventingv1alpha1.EventType {
 	return eventingv1alpha1.EventType{
 		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: fmt.Sprintf("%s-", toDNS1123Subdomain(spec.Type)),
+			GenerateName: fmt.Sprintf("%s-", utils.ToDNS1123Subdomain(spec.Type)),
 			Labels:       lbl,
 			Namespace:    namespace,
 		},
 		Spec: spec,
 	}
-}
-
-// Converts 'name' to a valid DNS1123 subdomain, required for object names in K8s.
-func toDNS1123Subdomain(name string) string {
-	// If it is not a valid DNS1123 subdomain, make it a valid one.
-	if msgs := validation.IsDNS1123Subdomain(name); len(msgs) != 0 {
-		// If the length exceeds the max, cut it and leave some room for a potential generated UUID.
-		if len(name) > validation.DNS1123SubdomainMaxLength {
-			name = name[:validation.DNS1123SubdomainMaxLength-generateNameSafety]
-		}
-		name = strings.ToLower(name)
-		name = validChars.ReplaceAllString(name, "")
-		// Only start/end with alphanumeric.
-		name = strings.Trim(name, "-.")
-	}
-	return name
 }
