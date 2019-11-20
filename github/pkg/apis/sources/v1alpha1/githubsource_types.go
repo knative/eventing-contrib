bbc3c4995b82c1832fd436677feb346cf10b8370
Lukas Berk <lberk@redhat.com>
2019-11-20 16:05:50 -0500

    Update deps

--- github/pkg/apis/sources/v1alpha1/githubsource_types.go
+++ github/pkg/apis/sources/v1alpha1/githubsource_types.go
@@ -25,7 +25,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"knative.dev/pkg/apis"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
-	apisv1alpha1 "knative.dev/pkg/apis/v1alpha1"
+	duckv1beta1 "knative.dev/pkg/apis/duck/v1beta1"
 )
 
 // Check that GitHubSource can be validated and can be defaulted.
@@ -69,7 +69,7 @@ type GitHubSourceSpec struct {
 	// Sink is a reference to an object that will resolve to a domain
 	// name to use as the sink.
 	// +optional
-	Sink *apisv1alpha1.Destination `json:"sink,omitempty"`
+	Sink *duckv1beta1.Destination `json:"sink,omitempty"`
 
 	// API URL if using github enterprise (default https://api.github.com)
 	// +optional
