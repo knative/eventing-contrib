c24df2f01ff5275959c45aeca3d4fa0af382d7b0
Scott Nichols <32305648+n3wscott@users.noreply.github.com>
2020-01-14 16:41:05 -0800

    Adding webhook support for github source. (#826)
    * adding webhook support for github source.
    
    * Upate custom columns to be a source.

---
+++ github/pkg/apis/sources/v1alpha1/githubsource_validation_test.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"knative.dev/pkg/webhook/resourcesemantics"
+
+	"knative.dev/pkg/apis"
+)
+
+func TestGitHubSourceValidation(t *testing.T) {
+	testCases := map[string]struct {
+		cr   resourcesemantics.GenericCRD
+		want *apis.FieldError
+	}{
+		"missing sink": {
+			cr: &GitHubSource{
+				Spec: GitHubSourceSpec{},
+			},
+			want: func() *apis.FieldError {
+				var errs *apis.FieldError
+				fe := apis.ErrMissingField("spec.sink")
+				errs = errs.Also(fe)
+				return errs
+			}(),
+		},
+	}
+
+	for n, test := range testCases {
+		t.Run(n, func(t *testing.T) {
+			got := test.cr.Validate(context.Background())
+			if diff := cmp.Diff(test.want.Error(), got.Error()); diff != "" {
+				t.Errorf("%s: validate (-want, +got) = %v", n, diff)
+			}
+		})
+	}
+}
