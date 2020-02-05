5806b8d0430969fc8cea03dee90285e94b4891e5
Nicola Ferraro <ni.ferraro@gmail.com>
2020-02-05 10:49:13 +0100

    Fix e2e tests for Camel K

--- test/e2e/camel_source_test.go
+++ test/e2e/camel_source_test.go
@@ -19,36 +19,27 @@ limitations under the License.
 package e2e
 
 import (
+	"context"
+	"testing"
+	"time"
+
+	camelv1alpha1 "github.com/apache/camel-k/pkg/apis/camel/v1alpha1"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientscheme "k8s.io/client-go/kubernetes/scheme"
 	"knative.dev/eventing-contrib/camel/source/pkg/apis/sources/v1alpha1"
 	camelsourceclient "knative.dev/eventing-contrib/camel/source/pkg/client/clientset/versioned"
 	"knative.dev/eventing/test/base/resources"
 	"knative.dev/eventing/test/common"
 	knativeduck "knative.dev/pkg/apis/duck/v1beta1"
-	"testing"
-	"time"
+	runtime "sigs.k8s.io/controller-runtime/pkg/client"
 )
-
-func createCamelSourceOrFail(c *common.Client, camelSource *v1alpha1.CamelSource) {
-	camelSourceClientSet, err := camelsourceclient.NewForConfig(c.Config)
-	if err != nil {
-		c.T.Fatalf("Failed to create CamelSource client: %v", err)
-	}
-
-	cSources := camelSourceClientSet.SourcesV1alpha1().CamelSources(c.Namespace)
-	if createdCamelSource, err := cSources.Create(camelSource); err != nil {
-		c.T.Fatalf("Failed to create CamelSource %q: %v", camelSource.Name, err)
-	} else {
-		c.Tracker.AddObj(createdCamelSource)
-	}
-}
 
 func TestCamelSource(t *testing.T) {
 
 	const (
 		camelSourceName = "e2e-camelsource"
-		loggerPodName = "e2e-camelsource-logger-pod"
-		body = "Hello, world!"
+		loggerPodName   = "e2e-camelsource-logger-pod"
+		body            = "Hello, world!"
 	)
 
 	client := common.Setup(t, true)
@@ -58,6 +49,14 @@ func TestCamelSource(t *testing.T) {
 	pod := resources.EventLoggerPod(loggerPodName)
 	client.CreatePodOrFail(pod, common.WithService(loggerPodName))
 
+	camelClient := getCamelKClient(client)
+
+	t.Logf("Creating Camel K IntegrationPlatform")
+	createCamelPlatformOrFail(client, camelClient, camelSourceName)
+
+	t.Logf("Creating Camel K Kit (to skip build)")
+	createCamelKitOrFail(client, camelClient, camelSourceName)
+
 	t.Logf("Creating CamelSource")
 	createCamelSourceOrFail(client, &v1alpha1.CamelSource{
 		ObjectMeta: meta.ObjectMeta{
@@ -66,11 +65,11 @@ func TestCamelSource(t *testing.T) {
 		Spec: v1alpha1.CamelSourceSpec{
 			Source: v1alpha1.CamelSourceOriginSpec{
 				Flow: &v1alpha1.Flow{
-					"from": &map[string]interface{} {
+					"from": &map[string]interface{}{
 						"uri": "timer:tick?period=1s",
-						"steps": []interface{} {
-							&map[string]interface{} {
-								"set-body": &map[string]interface{} {
+						"steps": []interface{}{
+							&map[string]interface{}{
+								"set-body": &map[string]interface{}{
 									"constant": body,
 								},
 							},
@@ -93,4 +92,74 @@ func TestCamelSource(t *testing.T) {
 	if err := client.CheckLog(loggerPodName, common.CheckerContains(body)); err != nil {
 		t.Fatalf("Strings %q not found in logs of logger pod %q: %v", body, loggerPodName, err)
 	}
+}
+
+func createCamelSourceOrFail(c *common.Client, camelSource *v1alpha1.CamelSource) {
+	camelSourceClientSet, err := camelsourceclient.NewForConfig(c.Config)
+	if err != nil {
+		c.T.Fatalf("Failed to create CamelSource client: %v", err)
+	}
+
+	cSources := camelSourceClientSet.SourcesV1alpha1().CamelSources(c.Namespace)
+	if createdCamelSource, err := cSources.Create(camelSource); err != nil {
+		c.T.Fatalf("Failed to create CamelSource %q: %v", camelSource.Name, err)
+	} else {
+		c.Tracker.AddObj(createdCamelSource)
+	}
+}
+
+func createCamelPlatformOrFail(c *common.Client, camelClient runtime.Client, camelSourceName string) {
+	platform := camelv1alpha1.IntegrationPlatform{
+		ObjectMeta: meta.ObjectMeta{
+			Name:      "camel-k",
+			Namespace: c.Namespace,
+		},
+	}
+
+	if err := camelClient.Create(context.TODO(), &platform); err != nil {
+		c.T.Fatalf("Failed to create IntegrationPlatform for CamelSource %q: %v", camelSourceName, err)
+	}
+}
+
+func createCamelKitOrFail(c *common.Client, camelClient runtime.Client, camelSourceName string) {
+	// Creating this kit manually because the Camel K platform is not configured to do it on its own.
+	// Testing that Camel K works is not in scope for this test.
+	kit := camelv1alpha1.IntegrationKit{
+		ObjectMeta: meta.ObjectMeta{
+			Name:      "test-kit",
+			Namespace: c.Namespace,
+			Labels: map[string]string{
+				"camel.apache.org/kit.type": "external",
+			},
+		},
+		Spec: camelv1alpha1.IntegrationKitSpec{
+			Dependencies: []string{
+				"camel:timer",
+				"mvn:org.apache.camel.k/camel-k-loader-knative",
+				"mvn:org.apache.camel.k/camel-k-loader-yaml",
+				"mvn:org.apache.camel.k/camel-k-runtime-knative",
+				"mvn:org.apache.camel.k/camel-k-runtime-main",
+			},
+			Image: "docker.io/testcamelk/camel-k-kit-bosp6ks52uhp0rb7beeg:3590738",
+		},
+	}
+
+	if err := camelClient.Create(context.TODO(), &kit); err != nil {
+		c.T.Fatalf("Failed to create IntegrationKit for CamelSource %q: %v", camelSourceName, err)
+	}
+}
+
+func getCamelKClient(c *common.Client) runtime.Client {
+	scheme := clientscheme.Scheme
+	scheme.AddKnownTypes(camelv1alpha1.SchemeGroupVersion, &camelv1alpha1.IntegrationPlatform{}, &camelv1alpha1.IntegrationPlatformList{})
+	scheme.AddKnownTypes(camelv1alpha1.SchemeGroupVersion, &camelv1alpha1.IntegrationKit{}, &camelv1alpha1.IntegrationKitList{})
+	meta.AddToGroupVersion(scheme, camelv1alpha1.SchemeGroupVersion)
+	options := runtime.Options{
+		Scheme: scheme,
+	}
+	client, err := runtime.New(c.Config, options)
+	if err != nil {
+		c.T.Fatalf("Failed to create initialize generic client for Camel K resources: %v", err)
+	}
+	return client
 }
