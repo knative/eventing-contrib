5f6758df44f01409306805aae8cb13ec6fd496cc
Nicola Ferraro <ni.ferraro@gmail.com>
2020-02-06 13:07:32 +0100

    CamelSource e2e test (2nd attempt) (#892)
    * make CamelSource OwnerRefable
    
    * CamelSource e2e test
    
    * Apply "global" transformations to camelk-operator from
    
    https://github.com/apache/camel-k/blob/1.0.0-RC1/pkg/install/operator.go#L63-L101
    
    * Remove duplicate roles in test/config/ camelk-operator yaml
    Add camel-k-builder-all-serviceaccounts granting builder role to all SAa
    
    * Fix e2e tests for Camel K
    
    * Fix test lib references
    
    * Update versions to camel.apache.org/v1
    
    * Fix usage of shared code
    
    Co-authored-by: Marek Schmidt <maschmid@redhat.com>

---
+++ test/e2e/camel_source_test.go
@@ -0,0 +1,165 @@
+//+build e2e
+
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	camelv1 "github.com/apache/camel-k/pkg/apis/camel/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sruntime "k8s.io/apimachinery/pkg/runtime"
+	"knative.dev/eventing-contrib/camel/source/pkg/apis/sources/v1alpha1"
+	camelsourceclient "knative.dev/eventing-contrib/camel/source/pkg/client/clientset/versioned"
+	"knative.dev/eventing/test/lib"
+	"knative.dev/eventing/test/lib/resources"
+	knativeduck "knative.dev/pkg/apis/duck/v1beta1"
+	runtime "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func TestCamelSource(t *testing.T) {
+
+	const (
+		camelSourceName = "e2e-camelsource"
+		loggerPodName   = "e2e-camelsource-logger-pod"
+		body            = "Hello, world!"
+	)
+
+	client := lib.Setup(t, true)
+	defer lib.TearDown(client)
+
+	t.Logf("Creating logger Pod")
+	pod := resources.EventLoggerPod(loggerPodName)
+	client.CreatePodOrFail(pod, lib.WithService(loggerPodName))
+
+	camelClient := getCamelKClient(client)
+
+	t.Logf("Creating Camel K IntegrationPlatform")
+	createCamelPlatformOrFail(client, camelClient, camelSourceName)
+
+	t.Logf("Creating Camel K Kit (to skip build)")
+	createCamelKitOrFail(client, camelClient, camelSourceName)
+
+	t.Logf("Creating CamelSource")
+	createCamelSourceOrFail(client, &v1alpha1.CamelSource{
+		ObjectMeta: meta.ObjectMeta{
+			Name: camelSourceName,
+		},
+		Spec: v1alpha1.CamelSourceSpec{
+			Source: v1alpha1.CamelSourceOriginSpec{
+				Flow: &v1alpha1.Flow{
+					"from": &map[string]interface{}{
+						"uri": "timer:tick?period=1s",
+						"steps": []interface{}{
+							&map[string]interface{}{
+								"set-body": &map[string]interface{}{
+									"constant": body,
+								},
+							},
+						},
+					},
+				},
+			},
+			Sink: &knativeduck.Destination{
+				Ref: resources.ServiceRef(loggerPodName),
+			},
+		},
+	})
+
+	t.Logf("Waiting for all resources ready")
+	client.WaitForAllTestResourcesReadyOrFail()
+
+	t.Logf("Sleeping for 3s to let the timer tick at least once")
+	time.Sleep(3 * time.Second)
+
+	if err := client.CheckLog(loggerPodName, lib.CheckerContains(body)); err != nil {
+		t.Fatalf("Strings %q not found in logs of logger pod %q: %v", body, loggerPodName, err)
+	}
+}
+
+func createCamelSourceOrFail(c *lib.Client, camelSource *v1alpha1.CamelSource) {
+	camelSourceClientSet, err := camelsourceclient.NewForConfig(c.Config)
+	if err != nil {
+		c.T.Fatalf("Failed to create CamelSource client: %v", err)
+	}
+
+	cSources := camelSourceClientSet.SourcesV1alpha1().CamelSources(c.Namespace)
+	if createdCamelSource, err := cSources.Create(camelSource); err != nil {
+		c.T.Fatalf("Failed to create CamelSource %q: %v", camelSource.Name, err)
+	} else {
+		c.Tracker.AddObj(createdCamelSource)
+	}
+}
+
+func createCamelPlatformOrFail(c *lib.Client, camelClient runtime.Client, camelSourceName string) {
+	platform := camelv1.IntegrationPlatform{
+		ObjectMeta: meta.ObjectMeta{
+			Name:      "camel-k",
+			Namespace: c.Namespace,
+		},
+	}
+
+	if err := camelClient.Create(context.TODO(), &platform); err != nil {
+		c.T.Fatalf("Failed to create IntegrationPlatform for CamelSource %q: %v", camelSourceName, err)
+	}
+}
+
+func createCamelKitOrFail(c *lib.Client, camelClient runtime.Client, camelSourceName string) {
+	// Creating this kit manually because the Camel K platform is not configured to do it on its own.
+	// Testing that Camel K works is not in scope for this test.
+	kit := camelv1.IntegrationKit{
+		ObjectMeta: meta.ObjectMeta{
+			Name:      "test-kit",
+			Namespace: c.Namespace,
+			Labels: map[string]string{
+				"camel.apache.org/kit.type": "external",
+			},
+		},
+		Spec: camelv1.IntegrationKitSpec{
+			Dependencies: []string{
+				"camel:timer",
+				"mvn:org.apache.camel.k/camel-k-loader-knative",
+				"mvn:org.apache.camel.k/camel-k-loader-yaml",
+				"mvn:org.apache.camel.k/camel-k-runtime-knative",
+				"mvn:org.apache.camel.k/camel-k-runtime-main",
+			},
+			Image: "docker.io/testcamelk/camel-k-kit-bosp6ks52uhp0rb7beeg:3590738",
+		},
+	}
+
+	if err := camelClient.Create(context.TODO(), &kit); err != nil {
+		c.T.Fatalf("Failed to create IntegrationKit for CamelSource %q: %v", camelSourceName, err)
+	}
+}
+
+func getCamelKClient(c *lib.Client) runtime.Client {
+	scheme := k8sruntime.NewScheme()
+	scheme.AddKnownTypes(camelv1.SchemeGroupVersion, &camelv1.IntegrationPlatform{}, &camelv1.IntegrationPlatformList{})
+	scheme.AddKnownTypes(camelv1.SchemeGroupVersion, &camelv1.IntegrationKit{}, &camelv1.IntegrationKitList{})
+	meta.AddToGroupVersion(scheme, camelv1.SchemeGroupVersion)
+	options := runtime.Options{
+		Scheme: scheme,
+	}
+	client, err := runtime.New(c.Config, options)
+	if err != nil {
+		c.T.Fatalf("Failed to create initialize generic client for Camel K resources: %v", err)
+	}
+	return client
+}
