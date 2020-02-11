3b5f20ea9240daa99735478a05eac568d604b07d
Matt Moore (via sockpuppet) <mattmoor@vmware.com>
2020-02-11 14:09:16 +0000

    Auto-update dependencies
    Produced via:
      `dep ensure -update knative.dev/test-infra knative.dev/pkg knative.dev/serving knative.dev/eventing`
    /assign n3wscott
    /cc n3wscott

--- vendor/knative.dev/eventing/test/lib/resources/eventing.go
+++ vendor/knative.dev/eventing/test/lib/resources/eventing.go
@@ -52,12 +52,21 @@ func channelRef(name string, typemeta *metav1.TypeMeta) *corev1.ObjectReference 
 	return pkgTest.CoreV1ObjectReference(typemeta.Kind, typemeta.APIVersion, name)
 }
 
+func KnativeRefForService(name, namespace string) *duckv1.KReference {
+	return &duckv1.KReference{
+		Kind:       "Service",
+		APIVersion: "v1",
+		Name:       name,
+		Namespace:  namespace,
+	}
+}
+
 // WithSubscriberForSubscription returns an option that adds a Subscriber for the given Subscription.
 func WithSubscriberForSubscription(name string) SubscriptionOption {
 	return func(s *messagingv1alpha1.Subscription) {
 		if name != "" {
 			s.Spec.Subscriber = &duckv1.Destination{
-				Ref: ServiceRef(name),
+				Ref: KnativeRefForService(name, ""),
 			}
 		}
 	}
@@ -68,7 +77,11 @@ func WithReplyForSubscription(name string, typemeta *metav1.TypeMeta) Subscripti
 	return func(s *messagingv1alpha1.Subscription) {
 		if name != "" {
 			s.Spec.Reply = &duckv1.Destination{
-				Ref: pkgTest.CoreV1ObjectReference(typemeta.Kind, typemeta.APIVersion, name),
+				Ref: &duckv1.KReference{
+					Kind:       typemeta.Kind,
+					APIVersion: typemeta.APIVersion,
+					Name:       name,
+					Namespace:  s.Namespace},
 			}
 		}
 	}
@@ -85,7 +98,7 @@ func WithDeadLetterSinkForSubscription(name string) SubscriptionOption {
 			}
 
 			delivery.DeadLetterSink = &duckv1.Destination{
-				Ref: ServiceRef(name),
+				Ref: KnativeRefForService(name, s.Namespace),
 			}
 
 		}
@@ -129,7 +142,7 @@ func WithDeliveryForBroker(delivery *eventingduckv1alpha1.DeliverySpec) BrokerOp
 	}
 }
 
-// ConfigMapPropagation returns a ConfigMapPropagation
+// ConfigMapPropagation returns a ConfigMapPropagation.
 func ConfigMapPropagation(name, namespace string) *configsv1alpha1.ConfigMapPropagation {
 	return &configsv1alpha1.ConfigMapPropagation{
 		ObjectMeta: metav1.ObjectMeta{
@@ -142,6 +155,22 @@ func ConfigMapPropagation(name, namespace string) *configsv1alpha1.ConfigMapProp
 				MatchLabels: resources.ConfigMapPropagationOwnedLabels(),
 			},
 		},
+	}
+}
+
+// ConfigMap returns a ConfigMap.
+func ConfigMap(name string, data map[string]string) *corev1.ConfigMap {
+	return &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Labels: map[string]string{
+				// Filter label for a configmap being picked to be propagated by current configmappropagation.
+				resources.CmpDefaultLabelKey: resources.CmpDefaultLabelValue,
+				// Default label for a configmap being eligible to be propagated.
+				"knative.dev/config-propagation": "original",
+			},
+		},
+		Data: data,
 	}
 }
 
@@ -199,7 +228,7 @@ func WithSubscriberKServiceRefForTrigger(name string) TriggerOption {
 	return func(t *eventingv1alpha1.Trigger) {
 		if name != "" {
 			t.Spec.Subscriber = duckv1.Destination{
-				Ref: ServiceRef(name),
+				Ref: KnativeRefForService(name, t.Namespace),
 			}
 		}
 	}
@@ -210,7 +239,7 @@ func WithSubscriberServiceRefForTrigger(name string) TriggerOption {
 	return func(t *eventingv1alpha1.Trigger) {
 		if name != "" {
 			t.Spec.Subscriber = duckv1.Destination{
-				Ref: ServiceRef(name),
+				Ref: KnativeRefForService(name, t.Namespace),
 			}
 		}
 	}
@@ -244,7 +273,7 @@ func WithDeadLetterSinkForDelivery(name string) DeliveryOption {
 	return func(delivery *eventingduckv1alpha1.DeliverySpec) {
 		if name != "" {
 			delivery.DeadLetterSink = &duckv1.Destination{
-				Ref: ServiceRef(name),
+				Ref: KnativeRefForService(name, ""),
 			}
 		}
 	}
