944755bc8c1597e9567a9936ecd61aeb61452828
Lukas Berk <lberk@redhat.com>
2020-02-12 16:40:18 -0500

    Update knative.dev/pkg dep in preperation for gen-reconciler work (#919)
    * Update pkg in preperation for gen-reconciler work
    
    * Update deps for serving, eventing, test-infra
    
    * run hack/update-deps.sh
    
    * Fix kafkasource tests

--- vendor/knative.dev/eventing/pkg/apis/messaging/v1beta1/channel_types.go
+++ vendor/knative.dev/eventing/pkg/apis/messaging/v1beta1/channel_types.go
@@ -17,11 +17,11 @@
 package v1beta1
 
 import (
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	eventingduck "knative.dev/eventing/pkg/apis/duck/v1beta1"
 	"knative.dev/pkg/apis"
+	duckv1 "knative.dev/pkg/apis/duck/v1"
 	"knative.dev/pkg/kmeta"
 )
 
@@ -73,8 +73,8 @@ type ChannelStatus struct {
 	// Channel conforms to ChannelableStatus
 	eventingduck.ChannelableStatus `json:",inline"`
 
-	// Channel is an ObjectReference to the Channel CRD backing this Channel.
-	Channel *corev1.ObjectReference `json:"channel,omitempty"`
+	// Channel is an KReference to the Channel CRD backing this Channel.
+	Channel *duckv1.KReference `json:"channel,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
