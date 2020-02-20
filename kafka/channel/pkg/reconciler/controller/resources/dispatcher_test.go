3a684ce96bd5226d03fa678279fa2308be93c983
Lionel Villard <villard@us.ibm.com>
2020-02-20 08:48:32 -0500

    slit SA and added ns dispatcher test

--- kafka/channel/pkg/reconciler/controller/resources/dispatcher_test.go
+++ kafka/channel/pkg/reconciler/controller/resources/dispatcher_test.go
@@ -35,6 +35,7 @@ func TestNewDispatcher(t *testing.T) {
 	os.Setenv(system.NamespaceEnvKey, "knative-testing")
 
 	args := DispatcherArgs{
+		DispatcherScope:     "cluster",
 		DispatcherNamespace: testNS,
 		Image:               imageName,
 	}
@@ -73,6 +74,91 @@ func TestNewDispatcher(t *testing.T) {
 							}, {
 								Name:  "CONFIG_LOGGING_NAME",
 								Value: "config-logging",
+							}},
+							Ports: []corev1.ContainerPort{{
+								Name:          "metrics",
+								ContainerPort: 9090,
+							}},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "config-kafka",
+									MountPath: "/etc/config-kafka",
+								},
+							},
+						}},
+					Volumes: []corev1.Volume{
+						{
+							Name: "config-kafka",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "config-kafka",
+									},
+								},
+							},
+						}},
+				},
+			},
+		},
+	}
+
+	got := MakeDispatcher(args)
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("unexpected condition (-want, +got) = %v", diff)
+	}
+}
+
+func TestNewNamespaceDispatcher(t *testing.T) {
+	os.Setenv(system.NamespaceEnvKey, "knative-testing")
+
+	args := DispatcherArgs{
+		DispatcherScope:     "namespace",
+		DispatcherNamespace: testNS,
+		Image:               imageName,
+	}
+
+	replicas := int32(1)
+	want := &v1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployments",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: testNS,
+			Name:      dispatcherName,
+		},
+		Spec: v1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: dispatcherLabels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: dispatcherLabels,
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: serviceAccountName,
+					Containers: []corev1.Container{
+						{
+							Name:  "dispatcher",
+							Image: imageName,
+							Env: []corev1.EnvVar{{
+								Name:  system.NamespaceEnvKey,
+								Value: "knative-testing",
+							}, {
+								Name:  "METRICS_DOMAIN",
+								Value: "knative.dev/eventing",
+							}, {
+								Name:  "CONFIG_LOGGING_NAME",
+								Value: "config-logging",
+							}, {
+								Name: "NAMESPACE",
+								ValueFrom: &corev1.EnvVarSource{
+									FieldRef: &corev1.ObjectFieldSelector{
+										FieldPath: "metadata.namespace",
+									},
+								},
 							}},
 							Ports: []corev1.ContainerPort{{
 								Name:          "metrics",
