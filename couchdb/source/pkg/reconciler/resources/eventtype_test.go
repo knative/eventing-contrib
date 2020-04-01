5af7f0c37205094b7b51640df4258e2585bbc2ba
slinkydeveloper <francescoguard@gmail.com>
2020-04-01 11:29:56 +0200

    Added test to fix coverage
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

---
+++ couchdb/source/pkg/reconciler/resources/eventtype_test.go
@@ -0,0 +1,62 @@
+package resources
+
+import (
+	"fmt"
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	eventingv1alpha1 "knative.dev/eventing/pkg/apis/eventing/v1alpha1"
+	"knative.dev/eventing/pkg/utils"
+	duckv1 "knative.dev/pkg/apis/duck/v1"
+	"knative.dev/pkg/kmp"
+
+	"knative.dev/eventing-contrib/couchdb/source/pkg/apis/sources/v1alpha1"
+)
+
+func TestMakeEventType(t *testing.T) {
+	eventSrc := &EventTypeArgs{
+		Src: &v1alpha1.CouchDbSource{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "source-test-namespace",
+				Name:      "source-test-name-",
+			},
+			Spec: v1alpha1.CouchDbSourceSpec{
+				ServiceAccountName: "test-service-account",
+				Sink: &duckv1.Destination{
+					Ref: &duckv1.KReference{
+						Name:       "test-sink",
+						Kind:       "Sink",
+						APIVersion: "duck.knative.dev/v1",
+					},
+				},
+			},
+		},
+		Type:   "test-type",
+		Source: "test-source",
+	}
+
+	result := MakeEventType(eventSrc)
+	want := &eventingv1alpha1.EventType{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: fmt.Sprintf("%s-", utils.ToDNS1123Subdomain("test-type")),
+			Labels:       Labels("source-test-name-"),
+			Namespace:    "source-test-namespace",
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(eventSrc.Src, schema.GroupVersionKind{
+					Group:   v1alpha1.SchemeGroupVersion.Group,
+					Version: v1alpha1.SchemeGroupVersion.Version,
+					Kind:    "KafkaSource",
+				}),
+			},
+		},
+		Spec: eventingv1alpha1.EventTypeSpec{
+			Type:   "type-test",
+			Source: "test-source",
+			Broker: "test-sink",
+		},
+	}
+	if diff, err := kmp.SafeDiff(result, want); err != nil {
+		t.Errorf("Unexpected eventtype: (-want, +got) = %v", diff)
+	}
+}
