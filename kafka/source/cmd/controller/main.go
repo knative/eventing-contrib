41568e27672946a168784b32aa4e2c55d5e28e16
Matt Moore <mattmoor@vmware.com>
2020-04-06 15:30:55 -0700

    Add a webhook for KafkaSource that runs alongside the controller.

--- kafka/source/cmd/controller/main.go
+++ kafka/source/cmd/controller/main.go
@@ -17,14 +17,91 @@ limitations under the License.
 package main
 
 import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	sourcesv1alpha1 "knative.dev/eventing-contrib/kafka/source/pkg/apis/sources/v1alpha1"
 	kafka "knative.dev/eventing-contrib/kafka/source/pkg/reconciler"
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
 	"knative.dev/pkg/injection/sharedmain"
+	"knative.dev/pkg/signals"
+	"knative.dev/pkg/webhook"
+	"knative.dev/pkg/webhook/certificates"
+	"knative.dev/pkg/webhook/resourcesemantics"
+	"knative.dev/pkg/webhook/resourcesemantics/defaulting"
+	"knative.dev/pkg/webhook/resourcesemantics/validation"
 )
 
 const (
 	component = "kafka-controller"
 )
 
+var types = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{
+	// List the types to validate.
+	sourcesv1alpha1.SchemeGroupVersion.WithKind("KafkaSource"): &sourcesv1alpha1.KafkaSource{},
+}
+
+func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	return defaulting.NewAdmissionController(ctx,
+
+		// Name of the resource webhook.
+		"defaulting.webhook.kafka.sources.knative.dev",
+
+		// The path on which to serve the webhook.
+		"/defaulting",
+
+		// The resources to validate and default.
+		types,
+
+		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
+		func(ctx context.Context) context.Context {
+			// Here is where you would infuse the context with state
+			// (e.g. attach a store with configmap data)
+			return ctx
+		},
+
+		// Whether to disallow unknown fields.
+		true,
+	)
+}
+
+func NewValidationAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	return validation.NewAdmissionController(ctx,
+
+		// Name of the resource webhook.
+		"validation.webhook.kafka.sources.knative.dev",
+
+		// The path on which to serve the webhook.
+		"/resource-validation",
+
+		// The resources to validate and default.
+		types,
+
+		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
+		func(ctx context.Context) context.Context {
+			// Here is where you would infuse the context with state
+			// (e.g. attach a store with configmap data)
+			return ctx
+		},
+
+		// Whether to disallow unknown fields.
+		true,
+	)
+}
+
 func main() {
-	sharedmain.Main(component, kafka.NewController)
+	ctx := webhook.WithOptions(signals.NewContext(), webhook.Options{
+		ServiceName: "kafka-source-webhook",
+		Port:        8443,
+		SecretName:  "kafka-source-webhook-certs",
+	})
+
+	sharedmain.WebhookMainWithContext(ctx, component,
+		certificates.NewController,
+		NewDefaultingAdmissionController,
+		NewValidationAdmissionController,
+
+		kafka.NewController,
+	)
 }
