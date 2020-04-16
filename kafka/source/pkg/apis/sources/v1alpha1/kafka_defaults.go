bbd84f0cf1287b568ec5ed862c8c4b004c88d249
Lukas Berk <lberk@redhat.com>
2020-04-16 10:32:48 -0400

    KafkaSource make consumergroup optional, set to uuid if not specified (#1142)
    * Make kafkasource consumergroup optional, generate uuid if not provided
    
    * Add unit tests
    
    * Add prefix to UUID

--- kafka/source/pkg/apis/sources/v1alpha1/kafka_defaults.go
+++ kafka/source/pkg/apis/sources/v1alpha1/kafka_defaults.go
@@ -18,9 +18,17 @@ package v1alpha1
 
 import (
 	"context"
+
+	"github.com/google/uuid"
+)
+
+const (
+	uuidPrefix = "knative-kafka-source-"
 )
 
 // SetDefaults ensures KafkaSource reflects the default values.
-func (r *KafkaSource) SetDefaults(ctx context.Context) {
-	// No defaults
+func (k *KafkaSource) SetDefaults(ctx context.Context) {
+	if k != nil && k.Spec.ConsumerGroup == "" {
+		k.Spec.ConsumerGroup = uuidPrefix + uuid.New().String()
+	}
 }
