260604f19841a1a459c2ad9c39cc7ab28926251d
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2020-06-04 10:02:03 +0200

    Disable consumer autocommit
    Signed-off-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- kafka/source/pkg/adapter/adapter.go
+++ kafka/source/pkg/adapter/adapter.go
@@ -91,6 +91,8 @@ func (a *Adapter) Start(stopCh <-chan struct{}) error {
 	if err != nil {
 		return fmt.Errorf("failed to create the config: %w", err)
 	}
+	config.Consumer.Offsets.AutoCommit.Enable = false
+
 	consumerGroupFactory := kafka.NewConsumerGroupFactory(addrs, config)
 	group, err := consumerGroupFactory.StartConsumerGroup(a.config.ConsumerGroup, a.config.Topics, a.logger, a)
 	if err != nil {
