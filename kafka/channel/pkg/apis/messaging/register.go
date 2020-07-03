4613c699dd3724149f564a27ccf087977d5bee04
Ali Ok <aliok@redhat.com>
2020-07-03 17:38:42 +0300

    Kafkachannel conversion webhook (#1374)
    * Webhook config for KafkaChannel v1beta1<>v1alpha1 conversion
    
    * Fix formatting issues
    
    * Fix RBAC issue

--- kafka/channel/pkg/apis/messaging/register.go
+++ kafka/channel/pkg/apis/messaging/register.go
@@ -16,6 +16,16 @@ limitations under the License.
 
 package messaging
 
+import "k8s.io/apimachinery/pkg/runtime/schema"
+
 const (
 	GroupName = "messaging.knative.dev"
+)
+
+var (
+	// KafkaChannelsResource represents a KafkaChannel
+	KafkaChannelsResource = schema.GroupResource{
+		Group:    GroupName,
+		Resource: "kafkachannels",
+	}
 )
