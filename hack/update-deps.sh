9cefcda1b2469a989df1e9377711b35f7ccb18a3
Nghia Tran <nghia@google.com>
2020-07-07 10:47:44 -0700

    Pin to knative.dev/eventing@release-16 (#1384)

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -24,15 +24,15 @@ source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/library.sh
 
 cd ${REPO_ROOT_DIR}
 
-VERSION="master"
+VERSION="release-0.16"
 
 # The list of dependencies that we track at HEAD and periodically
 # float forward in this repository.
 FLOATING_DEPS=(
-  "knative.dev/pkg@release-0.16"
+  "knative.dev/pkg@${VERSION}"
   "knative.dev/eventing@${VERSION}"
   "knative.dev/serving@${VERSION}"
-  "knative.dev/test-infra@release-0.16"
+  "knative.dev/test-infra@${VERSION}"
 )
 
 # Parse flags to determine any we should pass to dep.
