31dd3bb88024b95773af36b13efb146ddc08f5d4
Ali Ok <aliok@redhat.com>
2020-07-17 15:29:15 +0300

    Tmp change to debug stuff

--- test/e2e/broker_channel_flow_test.go
+++ test/e2e/broker_channel_flow_test.go
@@ -6,7 +6,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+   http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,21 +17,22 @@ limitations under the License.
 
 package e2e
 
-import (
-	"testing"
-
-	"knative.dev/eventing/test/e2e/helpers"
-)
-
-func TestBrokerChannelFlowTriggerV1BrokerV1(t *testing.T) {
-	helpers.BrokerChannelFlowWithTransformation(t, "MTChannelBasedBroker", "v1", "v1", channelTestRunner)
-}
-func TestBrokerChannelFlowV1Beta1BrokerV1(t *testing.T) {
-	helpers.BrokerChannelFlowWithTransformation(t, "MTChannelBasedBroker", "v1", "v1beta1", channelTestRunner)
-}
-func TestBrokerChannelFlowTriggerV1Beta1BrokerV1Beta1(t *testing.T) {
-	helpers.BrokerChannelFlowWithTransformation(t, "MTChannelBasedBroker", "v1beta1", "v1beta1", channelTestRunner)
-}
-func TestBrokerChannelFlowTriggerV1BrokerV1Beta1(t *testing.T) {
-	helpers.BrokerChannelFlowWithTransformation(t, "MTChannelBasedBroker", "v1beta1", "v1", channelTestRunner)
-}
+//
+//import (
+//	"testing"
+//
+//	"knative.dev/eventing/test/e2e/helpers"
+//)
+//
+//func TestBrokerChannelFlowTriggerV1BrokerV1(t *testing.T) {
+//	helpers.BrokerChannelFlowWithTransformation(t, "MTChannelBasedBroker", "v1", "v1", channelTestRunner)
+//}
+//func TestBrokerChannelFlowV1Beta1BrokerV1(t *testing.T) {
+//	helpers.BrokerChannelFlowWithTransformation(t, "MTChannelBasedBroker", "v1", "v1beta1", channelTestRunner)
+//}
+//func TestBrokerChannelFlowTriggerV1Beta1BrokerV1Beta1(t *testing.T) {
+//	helpers.BrokerChannelFlowWithTransformation(t, "MTChannelBasedBroker", "v1beta1", "v1beta1", channelTestRunner)
+//}
+//func TestBrokerChannelFlowTriggerV1BrokerV1Beta1(t *testing.T) {
+//	helpers.BrokerChannelFlowWithTransformation(t, "MTChannelBasedBroker", "v1beta1", "v1", channelTestRunner)
+//}
