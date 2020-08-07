a8befa127bcb01aac63c27ae30bde5c9b1885cc9
slinkydeveloper <francescoguard@gmail.com>
2020-08-07 09:45:05 +0200

    Reenable Test single event
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- test/e2e/channel_single_event_test.go
+++ test/e2e/channel_single_event_test.go
@@ -26,11 +26,9 @@ import (
 )
 
 func TestSingleBinaryEventForChannel(t *testing.T) {
-	t.SkipNow()
 	helpers.SingleEventForChannelTestHelper(t, cloudevents.EncodingBinary, "v1alpha1", "", channelTestRunner)
 }
 
 func TestSingleStructuredEventForChannel(t *testing.T) {
-	t.SkipNow()
 	helpers.SingleEventForChannelTestHelper(t, cloudevents.EncodingStructured, "v1alpha1", "", channelTestRunner)
 }
