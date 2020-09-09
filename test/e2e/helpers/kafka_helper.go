f0abe98537dc0b2b30168905883b588820b9bfa2
itsmurugappan <itsmurugappan@gmail.com>
2020-09-09 11:58:53 -0700

    use imc instead of kafka channel

--- test/e2e/helpers/kafka_helper.go
+++ test/e2e/helpers/kafka_helper.go
@@ -48,8 +48,8 @@ const (
 )
 
 var (
-	topicGVR        = schema.GroupVersionResource{Group: strimziApiGroup, Version: strimziApiVersion, Resource: strimziTopicResource}
-	ImcGVR = schema.GroupVersionResource{Group: "messaging.knative.dev", Version: "v1beta1", Resource: "inmemorychannels"}
+	topicGVR = schema.GroupVersionResource{Group: strimziApiGroup, Version: strimziApiVersion, Resource: strimziTopicResource}
+	ImcGVR   = schema.GroupVersionResource{Group: "messaging.knative.dev", Version: "v1beta1", Resource: "inmemorychannels"}
 )
 
 func MustPublishKafkaMessage(client *testlib.Client, bootstrapServer string, topic string, key string, headers map[string]string, value string) {
