55094575cbe0677a0685ea75419a40d749b58383
Matthias Wessendorf <mwessend@redhat.com>
2020-10-30 09:20:55 +0100

    Bump the parallel
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -202,7 +202,7 @@ function kafka_teardown() {
 
 initialize $@ --skip-istio-addon
 
-go_test_e2e -timeout=30m -parallel=12 ./test/e2e -channels=messaging.knative.dev/v1alpha1:KafkaChannel,messaging.knative.dev/v1beta1:KafkaChannel  || fail_test
+go_test_e2e -timeout=30m -parallel=20 ./test/e2e -channels=messaging.knative.dev/v1alpha1:KafkaChannel,messaging.knative.dev/v1beta1:KafkaChannel  || fail_test
 
 go_test_e2e -timeout=5m -parallel=12 ./test/conformance -channels=messaging.knative.dev/v1beta1:KafkaChannel -sources=sources.knative.dev/v1beta1:KafkaSource || fail_test
 
