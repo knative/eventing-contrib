505d781925889f2e6dd789eb368b78890a96b0f1
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2020-11-05 17:16:56 +0100

    [0.17] Update eventing to latest
    Signed-off-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- vendor/knative.dev/eventing/pkg/kncloudevents/message_sender.go
+++ vendor/knative.dev/eventing/pkg/kncloudevents/message_sender.go
@@ -25,15 +25,8 @@ import (
 
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/rickb777/date/period"
-	"go.opencensus.io/plugin/ochttp"
-	"knative.dev/pkg/tracing/propagation/tracecontextb3"
 
 	duckv1 "knative.dev/eventing/pkg/apis/duck/v1"
-)
-
-const (
-	defaultRetryWaitMin = 1 * time.Second
-	defaultRetryWaitMax = 30 * time.Second
 )
 
 var noRetries = RetryConfig{
@@ -46,41 +39,15 @@ var noRetries = RetryConfig{
 	},
 }
 
-// ConnectionArgs allow to configure connection parameters to the underlying
-// HTTP Client transport.
-type ConnectionArgs struct {
-	// MaxIdleConns refers to the max idle connections, as in net/http/transport.
-	MaxIdleConns int
-	// MaxIdleConnsPerHost refers to the max idle connections per host, as in net/http/transport.
-	MaxIdleConnsPerHost int
-}
-
-func (ca *ConnectionArgs) ConfigureTransport(transport *nethttp.Transport) {
-	if ca == nil {
-		return
-	}
-	transport.MaxIdleConns = ca.MaxIdleConns
-	transport.MaxIdleConnsPerHost = ca.MaxIdleConnsPerHost
-}
-
 type HttpMessageSender struct {
 	Client *nethttp.Client
 	Target string
 }
 
-func NewHttpMessageSender(connectionArgs *ConnectionArgs, target string) (*HttpMessageSender, error) {
-	// Add connection options to the default transport.
-	var base = nethttp.DefaultTransport.(*nethttp.Transport).Clone()
-	connectionArgs.ConfigureTransport(base)
-	// Add output tracing.
-	client := &nethttp.Client{
-		Transport: &ochttp.Transport{
-			Base:        base,
-			Propagation: tracecontextb3.TraceContextEgress,
-		},
-	}
-
-	return &HttpMessageSender{Client: client, Target: target}, nil
+// Deprecated: Don't use this anymore, now it has the same effect of NewHTTPMessageSenderWithTarget
+// If you need to modify the connection args, use ConfigureConnectionArgs sparingly.
+func NewHttpMessageSender(ca *ConnectionArgs, target string) (*HttpMessageSender, error) {
+	return &HttpMessageSender{Client: getClient(), Target: target}, nil
 }
 
 func (s *HttpMessageSender) NewCloudEventRequest(ctx context.Context) (*nethttp.Request, error) {
@@ -139,7 +106,12 @@ func (s *HttpMessageSender) SendWithRetries(req *nethttp.Request, config *RetryC
 		},
 	}
 
-	return retryableClient.Do(&retryablehttp.Request{Request: req})
+	retryableReq, err := retryablehttp.FromRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return retryableClient.Do(retryableReq)
 }
 
 func NoRetries() RetryConfig {
@@ -179,6 +151,6 @@ func RetryConfigFromDeliverySpec(spec duckv1.DeliverySpec) (RetryConfig, error) 
 	return retryConfig, nil
 }
 
-func checkRetry(_ context.Context, resp *nethttp.Response, _ error) (bool, error) {
-	return resp != nil && resp.StatusCode >= 300, nil
+func checkRetry(_ context.Context, resp *nethttp.Response, err error) (bool, error) {
+	return !(resp != nil && resp.StatusCode < 300), err
 }
